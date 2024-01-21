66747798cb22f05485db8b650ddec6d4152bf928
Martin Sagat <martins@autom8au.com.au>
2024-01-21 23:53:28 +0800

    add unit test for app module

---
+++ src/users/app.module.spec.ts
@@ -0,0 +1,28 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from './../app.module';
+import { AppController } from './../app.controller';
+import { AppService } from './../app.service';
+
+describe('AppModule', () => {
+  let appModule: TestingModule;
+
+  beforeAll(async () => {
+    appModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+  });
+
+  it('should be defined', () => {
+    expect(appModule).toBeDefined();
+  });
+
+  it('should have the AppController', () => {
+    const appController = appModule.get<AppController>(AppController);
+    expect(appController).toBeDefined();
+  });
+
+  it('should have the AppService', () => {
+    const appService = appModule.get<AppService>(AppService);
+    expect(appService).toBeDefined();
+  });
+});

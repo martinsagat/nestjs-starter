86c14fe0187b6a63787db7e42fd14074c452d3ab
Martin Sagat <martins@autom8au.com.au>
2024-01-21 22:59:22 +0800

    test jest coverage

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -33,57 +33,57 @@ describe('UsersService', () => {
     expect(usersService).toBeDefined();
   });
 
-  describe('create', () => {
-    it('should create a new user', async () => {
-      const role: Role = {
-        id: '1',
-        name: 'user',
-      };
+  // describe('create', () => {
+  //   it('should create a new user', async () => {
+  //     const role: Role = {
+  //       id: '1',
+  //       name: 'user',
+  //     };
 
-      await usersService.create(
-        {
-          email: 'test@test.com',
-          password: 'password',
-        },
-        [role],
-      );
+  //     await usersService.create(
+  //       {
+  //         email: 'test@test.com',
+  //         password: 'password',
+  //       },
+  //       [role],
+  //     );
 
-      expect(userRepository.save).toHaveBeenCalledTimes(1);
-      expect(userRepository.save).toHaveBeenCalledWith({
-        email: 'test@test.com',
-        password: 'password',
-        roles: [
-          {
-            id: '1',
-            name: 'user',
-          },
-        ],
-      });
-    });
+  //     expect(userRepository.save).toHaveBeenCalledTimes(1);
+  //     expect(userRepository.save).toHaveBeenCalledWith({
+  //       email: 'test@test.com',
+  //       password: 'password',
+  //       roles: [
+  //         {
+  //           id: '1',
+  //           name: 'user',
+  //         },
+  //       ],
+  //     });
+  //   });
 
-    it('should throw an error if user already exists', async () => {
-      userRepository.findOne = jest.fn().mockResolvedValue({
-        id: '1',
-        email: 'test@test.com',
-        password: 'password',
-      });
+  //   it('should throw an error if user already exists', async () => {
+  //     userRepository.findOne = jest.fn().mockResolvedValue({
+  //       id: '1',
+  //       email: 'test@test.com',
+  //       password: 'password',
+  //     });
 
-      await expect(
-        usersService.create(
-          {
-            email: 'test@test.com',
-            password: 'password',
-          },
-          [],
-        ),
-      ).rejects.toThrow('User with the same email already exists');
+  //     await expect(
+  //       usersService.create(
+  //         {
+  //           email: 'test@test.com',
+  //           password: 'password',
+  //         },
+  //         [],
+  //       ),
+  //     ).rejects.toThrow('User with the same email already exists');
 
-      expect(userRepository.findOne).toHaveBeenCalledTimes(1);
-      expect(userRepository.findOne).toHaveBeenCalledWith({
-        where: { email: 'test@test.com' },
-        withDeleted: true,
-      });
-      expect(userRepository.save).toHaveBeenCalledTimes(0);
-    });
-  });
+  //     expect(userRepository.findOne).toHaveBeenCalledTimes(1);
+  //     expect(userRepository.findOne).toHaveBeenCalledWith({
+  //       where: { email: 'test@test.com' },
+  //       withDeleted: true,
+  //     });
+  //     expect(userRepository.save).toHaveBeenCalledTimes(0);
+  //   });
+  // });
 });
